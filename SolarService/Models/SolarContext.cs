ee6bb33f7dce0188f3394ec8b43ebd1bcd70689f
Admin <Admin@IL707>
2020-06-22 09:14:06 +0300

    Add project files.

---
+++ SolarService/Models/SolarContext.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolarService.Models
+{
+    public class SolarContext : DbContext
+    {
+        public SolarContext()
+        {
+            if (Database.EnsureCreated())
+            {
+
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SolarDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            SolarStation st1 = new SolarStation { Id = 1, Name = "Station1" };
+            SolarStation st2 = new SolarStation { Id = 2, Name = "Station2" };
+            SolarStation st3 = new SolarStation { Id = 3, Name = "Station3" };
+
+            modelBuilder.Entity<SolarStation>().HasData(
+                new SolarStation[] { st1, st2, st3 });
+
+            modelBuilder.Entity<EventType>().HasData(
+                new EventType[]
+                {
+                new EventType { Id=1, Name="Error"},
+                new EventType { Id=2, Name="Normal event"}
+                });
+            //modelBuilder.Entity<Event>().HasData(
+            //    new Event[]
+            //    {
+            //        new Event {Id=1, Description="Invertor temperature above the normal", Date= DateTime.Now.AddDays(-2), }
+            //    });
+            modelBuilder.Entity<Invertor>().HasData(
+                new Invertor[]
+                {
+                new Invertor { Id=1, Name="Invertor1", Current=12.3, Power=100.5, ProducedEnergy=130.85, State="Working", StationId = st1.Id, Temperature=26.4, Voltage=20.9},
+                new Invertor { Id=2, Name="Invertor2", Current=16.3, Power=180.5, ProducedEnergy=160.85, State="Working", StationId = st2.Id, Temperature=29.4, Voltage=21.9},
+                new Invertor { Id=3, Name="Invertor3", Current=17.3, Power=200.5, ProducedEnergy=110.85, State="Working", StationId = st3.Id, Temperature=29.0, Voltage=22.9}
+                });
+
+            Role r1 = new Role() { Id = 1, Name = "Admin", RootRights = true };
+            Role r2 = new Role() { Id = 2, Name = "User", RootRights = false };
+
+            modelBuilder.Entity<Role>().HasData(
+                 new Role[] { r1, r2 });
+
+            modelBuilder.Entity<User>().HasData(
+                 new User[]
+                 {
+                new User { Id=1, Login="user1", Password="pass1", RoleId = r1.Id},
+                new User { Id=2, Login="user2", Password="pass2", RoleId = r2.Id},
+                new User { Id=3, Login="user3", Password="pass3", RoleId = r2.Id}
+                 });
+        }
+
+        public DbSet<SolarStation> SolarStations { get; set; }
+        public DbSet<Invertor> Invertors { get; set; }
+        public DbSet<EventType> EventTypes { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<TelegramAuthorisedUser> TelegramAuthorisedUsers { get; set; }
+    }
+}
---
+++ SolarService/Services/GreeterService.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using SolarService.Models;
+
+namespace SolarService
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        private SolarContext db;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+            db = new SolarContext();
+            Console.WriteLine("Service started. Database created");
+        }
+
+        public override async Task GetUsersAsync(EmptyRequest request, IServerStreamWriter<User> responseStream, ServerCallContext context)
+        {
+            List<User> users = db.Users.ToList();
+
+            foreach(User user in users)
+            {
+                await responseStream.WriteAsync(user);
+            }
+        }
+
+        public override async Task GetTelegramAuthorisedUsersAsync(EmptyRequest request, IServerStreamWriter<TelegramAuthorisedUser> responseStream, ServerCallContext context)
+        {
+            List<TelegramAuthorisedUser> users = db.TelegramAuthorisedUsers.ToList();
+
+            foreach (TelegramAuthorisedUser user in users)
+            {
+                await responseStream.WriteAsync(user);
+            }
+        }
+
+        public override async Task GetEventsAsync(EmptyRequest request, IServerStreamWriter<Event> responseStream, ServerCallContext context)
+        {
+            List<Event> events = db.Events.ToList();
+
+            foreach (Event item in events)
+            {
+                await responseStream.WriteAsync(item);
+            }
+        }
+
+        public override async Task GetEventTypesAsync(EmptyRequest request, IServerStreamWriter<EventType> responseStream, ServerCallContext context)
+        {
+            List<EventType> eventTypes = db.EventTypes.ToList();
+
+            foreach (EventType item in eventTypes)
+            {
+                await responseStream.WriteAsync(item);
+            }
+        }
+
+        public override async Task GetEventsByTypeAsync(EventsByTypeRequest request, IServerStreamWriter<Event> responseStream, ServerCallContext context)
+        {
+            List<Event> events = db.Events.Where(x => x.EventTypeId == request.TypeId).ToList();
+
+            foreach (Event item in events)
+            {
+                await responseStream.WriteAsync(item);
+            }
+        }
+
+        public override async Task GetStationsAsync(EmptyRequest request, IServerStreamWriter<SolarStation> responseStream, ServerCallContext context)
+        {
+            List<SolarStation> stations = db.SolarStations.ToList();
+
+            foreach (SolarStation item in stations)
+            {
+                await responseStream.WriteAsync(item);
+            }
+        }
+
+        public override async Task GetAllInvertorsAsync(EmptyRequest request, IServerStreamWriter<Invertor> responseStream, ServerCallContext context)
+        {
+            List<Invertor> invertors = db.Invertors.ToList();
+
+            foreach (Invertor item in invertors)
+            {
+                await responseStream.WriteAsync(item);  
+            }
+        }
+
+        public override async Task GetInvertorsOnStationAsync(InvertorsOnStationRequest request, IServerStreamWriter<Invertor> responseStream, ServerCallContext context)
+        {
+            List<Invertor> invertors = db.Invertors.Where(x => x.StationId == request.StationId).ToList();
+
+            foreach (Invertor item in invertors)
+            {
+                await responseStream.WriteAsync(item);
+            }
+        }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+
+        public override Task<StationProducedEnergy> GetStationProducedEnergyAsync(InvertorsOnStationRequest request, ServerCallContext context)
+        {
+            List<Invertor> invertors = db.Invertors.Where(x => x.StationId == request.StationId).ToList();
+            double totalEnergy = 0;
+            foreach (Invertor item in invertors)
+            {
+                totalEnergy += item.ProducedEnergy;
+            }
+            return Task.FromResult(new StationProducedEnergy()
+            {
+                Energy = totalEnergy
+            });
+        }
+    }
+}
